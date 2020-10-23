cedb22b06194769da1479c3b14d787d8f7160859
Satish Patel <iamsatishpatel1996@gmail.com>
2020-10-23 13:17:19 +0530

    docs : utils folder file

--- extra/utils/validation.js
+++ extra/utils/validation.js
@@ -1,42 +1,34 @@
-let users =
+const users =
   [
     {
       traineeEmail: "satish1.patel@successive.tech",
-      reviewerEmail: "shubham1.jain@successive.tech"
+      reviewerEmail: "shubham1.jain@successive.tech",
     },
     {
       traineeEmail: "satish2.patel@successive.tech",
-      reviewerEmail: "shubham2.jain@successive.tech"
+      reviewerEmail: "shubham2.jain@successive.tech",
     },
     {
-      traineeEmail: "satish13.patel@successive.tech",
-      reviewerEmail: "shubham13.jain@successive.tech"
+      traineeEmail: "satish13.patel@successive.tech1",
+      reviewerEmail: "shubham13.jain@successive.tech",
     },
     {
       traineeEmail: "satish14.patel@successive.tech",
-      reviewerEmail: "shubham4@.jain@successive.tech"
+      reviewerEmail: "shubham4@.jain@successive.tech",
     }
   ];
-let checkemail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const checkemail = /^[A-Za-z.0-9]{3,}@[A-Za-z]{10,10}[.]{1,1}[A-Za-z]{4,4}$/;
 let vldUser = [], inVldUser = [];
-function validateEmail(email) 
+validateEmail=(email)=>
 {
   return checkemail.test(String(email).toLowerCase());
 }
-
-function validateUsers(user) 
+function validateUsers(userArray) 
 {
-  user.forEach((element) =>
+  userArray.forEach((element) =>
   {
     let { traineeEmail, reviewerEmail }=element;
-    if (validateEmail(traineeEmail) && validateEmail(reviewerEmail)) 
-    {
-      vldUser.push(element);
-    }
-    else
-    {
-      inVldUser.push(element);
-    }
+    (validateEmail(traineeEmail) && validateEmail(reviewerEmail)) ? vldUser.push(element) : inVldUser.push(element);
 });
 console.log("Total valid users : ", vldUser.length);
 console.log("valid users are : ", vldUser);
