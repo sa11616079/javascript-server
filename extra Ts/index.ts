c3d45c9b3d7b8c61a83d473b2a92b9dadf9bce0c
Satish Patel <iamsatishpatel1996@gmail.com>
2020-10-26 15:00:42 +0530

    docs :  files

--- extra Ts/index.ts
+++ extra Ts/index.ts
@@ -1,26 +1,4 @@
-// import {diamond} from './pattern/index.js';
-// import {equilateral} from './pattern/index.js';
-// import {hasPermission} from './utils/index.js';
-// import {permissions} from './constants.js';
-// import {validateUsers} from './utils/index.js';
-// import {users} from './utils/helpers.js';
-
-// import { IUsers } from "./interfaces";
 import { validateUsers } from "./utils/index";
-
-// diamond(5);
-// diamond(10);
-// console.log();
-// equilateral(5);
-// equilateral(10);
-// console.log();
-// hasPermission(permissions.getUser1, "trainer", "delete");
-// hasPermission(permissions.getUser1, "trainee", "read");
-// console.log();
-// validateUsers(users);
-
-
-//helper.js
 
 const users: IUsers[] =
   [
@@ -38,7 +16,7 @@ const users: IUsers[] =
     },
     {
       traineeEmail: "satish14.patel@successive.tech",
-      reviewerEmail: "shubham4@.jain@successive.tech"
+      reviewerEmail: "shubham14@.jain@successive.tech"
     }
   ];
 
