c4dc88eb335428b5f8b091c751798eccd72705a3
Satish Patel <iamsatishpatel1996@gmail.com>
2020-10-27 12:26:56 +0530

    delete node_modules files

--- dist/config/configuration.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const envVars = require('dotenv').config();
-console.log("inside config", envVars);
-const config = envVars.parsed;
-exports.default = config;
-Object.freeze(config);
-//# sourceMappingURL=configuration.js.map
