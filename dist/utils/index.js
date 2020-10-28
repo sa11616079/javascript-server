ac7b20cfcd5a5599be669695272d64160744dce4
sa11616079 <satish.patel@successive.tech>
2020-10-28 18:15:14 +0530

    docs : .gitignore files

--- dist/utils/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateUsers = exports.hasPermission = void 0;
-const permissions_js_1 = require("./permissions.js");
-Object.defineProperty(exports, "hasPermission", { enumerable: true, get: function () { return permissions_js_1.hasPermission; } });
-const validation_js_1 = require("./validation.js");
-Object.defineProperty(exports, "validateUsers", { enumerable: true, get: function () { return validation_js_1.validateUsers; } });
-//index.js
-//# sourceMappingURL=index.js.map
